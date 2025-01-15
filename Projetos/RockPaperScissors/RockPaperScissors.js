af2c6b5e3aae5a154fbd43b22a469ab0acb0997c
void-Pedro <pedroborges.itapira@gmail.com>
2025-01-15 19:47:54 -0300

    Testes iniciais

--- Projetos/RockPaperScissors/RockPaperScissors.js
+++ Projetos/RockPaperScissors/RockPaperScissors.js
@@ -1,54 +1,67 @@
-let playerScore = 0
-let computerScore = 0
-let roundWinner = ''
+let playerScore = 0;
+let computerScore = 0;
+let blocked = false;
 
-const playerSelection = "Rock";
+const emojiComp = document.querySelector(".comp");
+const gameResult = document.querySelector(".gameresult");
+const placarAtual = document.querySelector(".atual");
 
 function getComputerChoice() {
-    i = Math.floor(Math.random() * 3)
-    if (i == 0) {
-        return "Rock";
-    } else if (i == 1) {
-        return "Paper";
-    } else {
-        return "Scissors"
-    }
+  i = Math.floor(Math.random() * 3)
+  if (i == 0) {
+      return "Rock";
+  } else if (i == 1) {
+      return "Paper";
+  } else {
+      return "Scissors";
+  }
 }
 
-function playRound(playerSelection, computerSelection) {
+function atualizaPlacar() {
+  placarAtual.innerHTML = playerScore + " x " + computerScore;
+}
+
+function atualizaTexto(computerSelection, resultado) {
+  gameResult.innerHTML = "O computador joga " + computerSelection + ". " + resultado;
+}
+
+function clickEmoji(playerSelection) {
+  let texto = "";
+  let computerSelection = getComputerChoice();
+  if(blocked != true) {
     if (playerSelection === computerSelection) {
-      roundWinner = 'tie'
+      computerScore++;
+      playerScore++;
+      texto = "É um empate!";
     }
     if (
       (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
       (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
       (playerSelection === 'Paper' && computerSelection === 'Rock')
     ) {
-      playerScore++
-      roundWinner = 'player'
+      playerScore++;
+      texto = 'O player ganhou o round!';
     }
     if (
       (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
       (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
       (computerSelection === 'Paper' && playerSelection === 'Rock')
     ) {
-      computerScore++
-      roundWinner = 'computer'
+      computerScore++;
+      texto = 'O computador ganhou o round!';
     }
   }
-
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Computer plays " + computerSelection + " " + roundWinner + " wins!");
-    }
-    if (playerScore == 5) {
-        console.log("Player Wins!!")
+  if((playerScore == 5) || (computerScore == 5)) {
+    blocked = true;
+    if(playerScore == 5) {
+      let vencedor = "player"
     } else {
-        console.log("Computer Wins!!")
+      vencedor = "computador"
     }
-    
+    gameResult.innerHTML += "<br>O " + vencedor + " vence o jogo!!";
+  } else {
+    atualizaPlacar();
+    atualizaTexto(computerSelection, texto);
+  }
+  
 }
-
-game()
